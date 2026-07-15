d59d29d9b24c91826836a8caa0d0a29d67214939
nobinbabu94 <nobins73@gmail.com>
2026-07-15 23:09:35 +0530

    gtm added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 // const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 import { Poppins } from "next/font/google";
 
@@ -79,6 +80,17 @@ export default function RootLayout({ children }) {
     <html lang="en" className={poppins.variable}>
       <head>
         <link rel="canonical" href="https://best-movers.in" />
+        {GTM_ID && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`,
+            }}
+          />
+        )}
         {/* JSON-LD Schema */}
         <script
           type="application/ld+json"
@@ -134,6 +146,17 @@ export default function RootLayout({ children }) {
       </head>
 
       <body className="antialiased">
+        {GTM_ID && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        )}
+
         {/* Google Analytics */}
         {/* {GA_ID && (
           <>
